d0255fd1760971630dca71a4be688ee720544e61
mihelchris <mihelchris@gmail.com>
2022-04-03 12:04:20 +0800

    Added Week 10

---
+++ src/components/prototypes/MentalHealthTwo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FigmaEmbed from 'react-figma-embed';
+ 
+const MentalHealthTwo = () => {
+    return (
+      <FigmaEmbed 
+      style={{
+          width: "100%"
+      }}
+      url="https://www.figma.com/proto/CsZzRq5V5Ue26HLLrs4q3X/Mental-Support-Community?node-id=391:7534" />
+    );
+}
+
+export default MentalHealthTwo
